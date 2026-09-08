a9c0b8281642c0a88f52be436284a036301cb55b
Nicolas <nicolas.launay.0@gmail.com>
2026-09-08 14:49:55 +0200

    EDIT: added missing properties to Skill interface

--- src/domain/entities/skill.ts
+++ src/domain/entities/skill.ts
@@ -1,6 +1,8 @@
-// TODO : fichier Kotlin d'origine (Skill.kt) non fourni -> version minimale à confirmer.
 export interface Skill {
   readonly id: number
   readonly name: string
+  readonly level: number
   readonly description: string
+  readonly isLimited: boolean
+  readonly isSpell: boolean
 }
