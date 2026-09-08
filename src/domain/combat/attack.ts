69694fd9bac03adf2639f402f02ff31166a950e7
Nicolas <nicolas.launay.0@gmail.com>
2026-09-08 14:49:11 +0200

    EDIT: added attackDamage -> string formatting function

--- src/domain/combat/attack.ts
+++ src/domain/combat/attack.ts
@@ -1,11 +1,22 @@
 import type { Stat } from "../entities/stat";
-import type { Damage } from "./damage";
+import { formatDamage, type Damage } from "./damage";
 
 export interface Attack {
   name: string,
   attackStat: Stat,
   damageThrows: Damage[],
   description: string,
+}
+
+export function formatAttackDamage(attack: Attack): string {
+  let damages = ''
+
+  attack.damageThrows.forEach((dmg, index) => {
+    damages += index == 0 ? '' : ' + '
+    damages += formatDamage(dmg)
+  })
+
+  return damages
 }
 
 
