bd6d0aa35eb5a3ea82ac871133d6ae939d847b02
Nicolas <nicolas.launay.0@gmail.com>
2026-09-13 16:02:43 +0200

    NEW: added throwDice method

--- src/domain/combat/dice-type.ts
+++ src/domain/combat/dice-type.ts
@@ -1,11 +1,18 @@
+import { type PositiveInteger, toPositiveInteger } from '../positive-integer'
+
 export const DICE_TYPES = {
-  D4: {displayName: 'd4'},
-  D6: {displayName: 'd6'},
-  D8: {displayName: 'd8'},
-  D10: {displayName: 'd10'},
-  D12: {displayName: 'd12'},
-  D20: {displayName: 'd20'},
-  D100: {displayName: 'd100'},
+  D4: { displayName: 'd4' },
+  D6: { displayName: 'd6' },
+  D8: { displayName: 'd8' },
+  D10: { displayName: 'd10' },
+  D12: { displayName: 'd12' },
+  D20: { displayName: 'd20' },
+  D100: { displayName: 'd100' },
 } as const
 
 export type DiceType = keyof typeof DICE_TYPES
+
+export function throwDice(dice: DiceType): PositiveInteger {
+  const diceMax = Number(dice.slice(1))
+  return toPositiveInteger(Math.floor(Math.random() * diceMax + 1))
+}
--- src/domain/positive-integer.ts
+++ src/domain/positive-integer.ts
@@ -1,13 +1,13 @@
-export type PositiveInteger = number & { readonly __brand: 'PositiveInteger' };
+export type PositiveInteger = number & { readonly __brand: 'PositiveInteger' }
 
 export function isPositiveInteger(value: number): boolean {
   return Number.isInteger(value) && value >= 0
 }
 
-// La fonction qui valide et "marque" la valeur
+// validates and marks value
 export function toPositiveInteger(value: number): PositiveInteger {
   if (!isPositiveInteger(value)) {
-    throw new Error(`Entier positif attendu, reçu : ${value}`);
+    throw new Error(`Entier positif attendu, reçu : ${value}`)
   }
-  return value as PositiveInteger;
+  return value as PositiveInteger
 }
