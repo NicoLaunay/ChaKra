235b096fb5d200fb12c0f507fa0c17c8c6fffb09
Nicolas <nicolas.launay.0@gmail.com>
2026-09-15 10:44:47 +0200

    NEW: created and implemented Burger component prototype

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,21 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import Burger from './ui/components/Burger.vue'
+</script>
 
 <template>
-  <div id="header">
+  <div
+    id="header"
+    class="flex justify-between items-center mb-5 px-5 py-4 border-b border-b-secondary"
+  >
     <router-link to="/" class="flex gap-1 items-center">
       <img src="./ui/assets/app-logo-small.png" alt="logo" />
       <h1 class="text-primary font-bold text-xl">ChaKra</h1>
     </router-link>
+    <Burger />
   </div>
-  <router-view />
+  <div class="px-5">
+    <router-view />
+  </div>
 </template>
 
 <style scoped></style>
