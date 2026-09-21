15e695da55e9a666a87f4034cb54f16d67585263
Nicolas <nicolas.launay.0@gmail.com>
2026-09-21 15:06:08 +0200

    EDIT: created PlayerCharacter Builder

--- src/domain/entities/player-character.ts
+++ src/domain/entities/player-character.ts
@@ -1,5 +1,7 @@
-import type { Entity } from './entity'
+import { toPositiveInteger } from '../positive-integer'
+import { EntityBuilder, type Entity } from './entity'
 import type { Player } from './player'
+import { Pool } from './pool'
 import type { Profile } from './profile'
 
 export interface PlayerCharacter {
@@ -7,10 +9,44 @@ export interface PlayerCharacter {
   readonly player: Player
   readonly profile: Profile
 
-  readonly pcMax: number
-  readonly pc: number
-  readonly prMax: number
-  readonly pr: number
-  readonly pmMax: number
-  readonly pm: number
+  readonly pc: Pool
+  readonly pr: Pool
+  readonly pm: Pool
+}
+
+export class PlayerCharacterBuilder implements PlayerCharacter {
+  entity: Entity = new EntityBuilder().build()
+  player: Player = { name: 'Unknown Player' }
+  profile: Profile = {
+    id: 0,
+    name: 'void profile',
+    lifeDice: 'D4',
+    paths: [],
+    starterWeapons: [],
+    starterArmors: [],
+  }
+  pc: Pool = Pool.create(toPositiveInteger(10))
+  pr: Pool = Pool.create(toPositiveInteger(10))
+  pm: Pool = Pool.create(toPositiveInteger(0))
+
+  build() {
+    return {
+      entity: this.entity,
+      player: this.player,
+      profile: this.profile,
+      pc: this.pc,
+      pr: this.pr,
+      pm: this.pm,
+    }
+  }
+
+  withPlayer(player: Player): this {
+    this.player = player
+    return this
+  }
+
+  withName(name: string): this {
+    this.entity.name = name
+    return this
+  }
 }
