5a136922138af386ee5cf528fe5abb787e999eed
Nicolas <nicolas.launay.0@gmail.com>
2026-09-21 15:02:49 +0200

    EDIT: LineCharacter builder now complete with properties initializers and conversion function from PlayerCharacter

--- src/ui/components/display/line-character.ts
+++ src/ui/components/display/line-character.ts
@@ -1,25 +1,70 @@
+import type { PlayerCharacter } from '@/domain'
+
 export interface LineCharacter {
   icon: string
+  player: string
   name: string
   profile: string
   race: string
   level: number
 }
 
-export class LineCharacterBuilder implements LineCharacter {
-  icon: string = '../assets/app-logo-small.png'
-  name: string = 'Personnage Anonyme'
-  profile: string = 'Guerrier'
-  race: string = 'Humain'
-  level: number = 1
+export class LineCharacterBuilder {
+  private icon = '../assets/app-logo-small.png'
+  private player = 'Joueur Anonyme'
+  private name = 'Personnage Anonyme'
+  private profile = 'Guerrier'
+  private race = 'Humain'
+  private level = 1
+
+  withIcon(icon: string): this {
+    this.icon = icon
+    return this
+  }
+
+  withPlayer(player: string): this {
+    this.player = player
+    return this
+  }
+
+  withName(name: string): this {
+    this.name = name
+    return this
+  }
+
+  withProfile(profile: string): this {
+    this.profile = profile
+    return this
+  }
+
+  withRace(race: string): this {
+    this.race = race
+    return this
+  }
+
+  withLevel(level: number): this {
+    this.level = level
+    return this
+  }
 
   build(): LineCharacter {
     return {
       icon: this.icon,
+      player: this.player,
       name: this.name,
       profile: this.profile,
       race: this.race,
       level: this.level,
     }
   }
+}
+
+export function toLineCharacter(character: PlayerCharacter): LineCharacter {
+  return new LineCharacterBuilder()
+    .withName(character.entity.name)
+    .withPlayer(character.player.name)
+    .withProfile(character.profile.name)
+    .withRace(character.entity.race?.name || '-')
+    .withLevel(character.entity.level)
+    .build()
 }
