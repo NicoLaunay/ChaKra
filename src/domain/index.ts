6e7886fc65a5c1cd97c07205352faa672f2cdaba
Nicolas <nicolas.launay.0@gmail.com>
2026-09-21 15:05:42 +0200

    REFACTOR: created barrels

---
+++ src/domain/index.ts
@@ -0,0 +1,30 @@
+export * from './positive-integer'
+export * from './errors'
+
+// combat
+export * from './combat/damage'
+export * from './combat/damage-type'
+export * from './combat/dice-type'
+export * from './combat/attack'
+export * from './combat/weapon'
+export * from './combat/armor'
+
+// entities
+export * from './entities/entity'
+export * from './entities/player-character'
+export * from './entities/pool'
+export * from './entities/stat-block'
+export * from './entities/stat'
+export * from './entities/sex'
+export * from './entities/entity-type'
+export * from './entities/entity-size'
+export * from './entities/player'
+export * from './entities/profile'
+export * from './entities/race'
+export * from './entities/racial-ability'
+export * from './entities/path'
+export * from './entities/entity-path'
+export * from './entities/skill'
+
+// inventory
+export * from './inventory/purse'
